b6be9267c9aa5d4b0da27451f1220c1b7a64feb5
Trent <fittytrent@gmail.com>
2015-11-08 00:35:31 -0600

    Start

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Step one
+Install all dependencies with `npm install`.
+
+
+# Step two
+Run either of the following to start the app:
+
+`npm start`
+
+*OR*
+
+`DEBUG=gifsmas:* npm start`
+
+# Step three
+Point your browser to [http://localhost:3000].
---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var hbs = require('express-hbs');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var server = express();
+
+// view engine setup
+
+// Use `.hbs` for extensions and find partials in `views/partials`.
+server.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials',
+  layoutsDir: __dirname + '/views/layouts/',
+  defaultLayout: __dirname + '/views/layouts/main.hbs',
+}));
+server.set('views', path.join(__dirname, 'views'));
+//server.engine('hbs', exphbs({defaultLayout: 'layout', extname: ".hbs"}));
+server.set('view engine', 'hbs');
+
+hbs.registerAsyncHelper('svg', function(filename, cb) {
+  var tmp = path.join(__dirname, "public", filename);
+  fs.readFile(tmp, 'utf8', function(err, content) {
+    console.log(content);
+    cb(new hbs.SafeString(content));
+  });
+});
+
+
+// uncomment after placing your favicon in /public
+server.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+server.use(logger('dev'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(cookieParser());
+server.use(require('node-sass-middleware')({
+  src: '/scss',
+  dest: '/css',
+  indentedSyntax: false, //impacts scss/sass
+  sourceMap: true,
+  debug: true,
+  root: path.join(__dirname, 'public'),
+}));
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.use('/', routes);
+server.use('/users', users);
+
+// catch 404 and forward to error handler
+server.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (server.get('env') === 'development') {
+  server.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+server.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = server;
