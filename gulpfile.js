7d642335e7bc84d8d615cbd0a0c897a36302b027
Trent <fittytrent@gmail.com>
2015-11-11 17:52:23 -0600

    Generate js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cache = require('gulp-cache');
-var livereload = require('gulp-livereload');
 
 
 gulp.task('styles', function(){
@@ -12,29 +11,22 @@ gulp.task('styles', function(){
     .pipe(sass())
     .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest('public/css/'))
-    .pipe(livereload())
 });
 
 gulp.task('scripts', function(){
-  return gulp.src('public/app/**/*.js')
+  return gulp.src(['public/app/**/*.js', 'public/app/common/**/*.js'])
     .pipe(concat('main.js'))
     .pipe(gulp.dest('public/js/'))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest('public/js/'))
-    .pipe(livereload())
 });
 
-gulp.task('reload', function(){
-  livereload();
-})
-
 gulp.task('default', function(){
-   livereload.listen();
   // making gulp generate map files takes a few additional steps
   // so we're continueing to use express for now.
   //gulp.watch("public/scss/**/*.scss", ['styles']);
-  gulp.watch('public/scss/*.scss', ['reload']);
+  gulp.watch('public/scss/*.scss', ['scripts']);
   gulp.watch("public/app/**/*.js", ['scripts']);
-  gulp.watch("public/app/**/*.html", ['reload']);
+  gulp.watch("public/app/**/*.html", ['scripts']);
 });
